58beec3351ba5578b437e2a262c5dd34c27ba63b
Robin Xia <rx2141@nyu.edu>
2023-03-27 16:49:14 -0400

    update call.py

--- lab1/call.py
+++ lab1/call.py
@@ -2,15 +2,21 @@ import os
 
 base_command = "(time ./maxnum {} {} input10_{}) >  outlog 2>&1"
 
+def convert_time_to_ms(time_str):
+    minutes, seconds = time_str.strip().split('m')
+    seconds = seconds.strip('s')
+    total_seconds = float(minutes) * 60 + float(seconds)
+    return total_seconds * 1000
+
 def extract_time(filename):
     with open(filename, 'r') as f:
         # read the file and split it into lines
         lines = f.read().splitlines()
 
         # extract the real, user, and sys time from the lines
-        real_time = lines[-3].split()[1]
-        user_time = lines[-2].split()[1]
-        sys_time = lines[-1].split()[1]
+        real_time = convert_time_to_ms(lines[-3].split()[1])
+        user_time = convert_time_to_ms(lines[-2].split()[1])
+        sys_time = convert_time_to_ms(lines[-1].split()[1])
 
         # return the times as a dictionary
         return {'real': real_time, 'user': user_time, 'sys': sys_time}
@@ -25,7 +31,8 @@ def print_table(data):
     print('|'.join(headers))
 
     # print the horizontal line separator
-    print('-' * sum(widths + [len(headers) * 3 - 1]))
+   # print('-' * sum(widths + [len(headers) * 3 - 1]))
+    print('-' * (sum(widths) + len(headers) * 3 - 1))
 
     # print the data rows
     for row in data:
@@ -56,12 +63,15 @@ for thread_num in threads_list:
         command = base_command.format(thread_num, len_num, suffix)
         os.system(command)
         times = extract_time('outlog')
-        data1[idx].append(str(times['real']))
-        data2[idx].append(str(times['user'] + times['sys']))
+        data1[idx].append(str(times['real']) + "ms")
+        data2[idx].append(str(times['user'] + times['sys']) + "ms")
     idx += 1
     suffix += 1
 
+
+print("\n")
 print_table(data1)
+print("\n")
 print_table(data2)
 
 
