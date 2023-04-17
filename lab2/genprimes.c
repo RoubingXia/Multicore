5203e0a3568d90962773509bc414d4735e0b9ee4
Robin Xia <rx2141@nyu.edu>
2023-04-17 11:54:33 -0400

    parallel copy to res part

--- lab2/genprimes.c
+++ lab2/genprimes.c
@@ -2,42 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <omp.h>
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <omp.h>
 
 
-
-
-
-int main() {
-    int num_threads = 4;
-    int queue_size = 16;
-    Queue q;
-    init_queue(&q, queue_size);
-
-    // Enqueue some values
-    for (int i = 0; i < queue_size; i++) {
-        enqueue(&q, i);
-    }
-
-    // Consume the queue using OpenMP
-    #pragma omp parallel num_threads(num_threads)
-    {
-        int value;
-        while (!is_empty(&q)) {
-            value = dequeue(&q);
-            printf("Thread %d consumed %d\n", omp_get_thread_num(), value);
-        }
-    }
-
-    return 0;
-}
-
- * */
-
-// A simple queue implementation with a single mutex
+// A simple queue implementation
 typedef struct {
     int* data;
     int front;
@@ -90,6 +57,7 @@ void write_helper(char file_name[], int primes[], int n) {
     fclose(fptr);
 }
 
+// Sequential algorithm
 int* getPrimes(int n, int* size) {
     int* candidates = (int*)malloc(sizeof(int)*(n + 1));
     for (int i = 0; i <= n; ++i) {
@@ -123,34 +91,20 @@ int* getPrimes(int n, int* size) {
     return res;
 }
 
-int findNext(int thread_num, int thread_count, int n, int cur, int* candidates) {
-    // This function should find the next unchecked prime number for thread_num
-    // Algorithm: 1. Start from cur + 1, iterate the candidates every time meet a candidates[i] == 1, do count++
-    //            2. When count == (thread_count - thread_num), that means this is the next start prime for thread_num
-    int count = 0;
-    int dest = thread_count;
-    int next = cur;
-    while(count < dest && next < n) {
-        next++;
-        if (candidates[next] == 1) {
-            count++;
-        }
-    }
-    return next == cur ? -1 : next;// -1 means no primes left for check
-}
+
+// Parallel algorithm
 void getPrimesM(int n, int* size, int threads_count, int** res) {
-    // each thread hand le a chunk of input range
     int* candidates = (int*)malloc(sizeof(int)*(n + 1));
     int res_count = 0; // total prime numbers
     int step = n / threads_count;
     // get the starting point for each thread
     int len1 = 0;// length of start_points
-    int* start_points = getPrimes((n + 1) / 2, &len1); // find the first half primes, use them as starting points for each thread
+    int* start_points = getPrimes((int)(sqrt(n)), &len1); // find the start primes, use them as starting points for each thread
     int local_count[threads_count]; // use to count the number of primes in each chunk
     int queue_size = len1;
     Queue q;
     init_queue(&q, queue_size);
-    // en queue the first 100 primes
+    // En queue the start primes
     for (int i = 0; i < queue_size; i++) {
         enqueue(&q, start_points[i]);
     }
@@ -163,11 +117,10 @@ void getPrimesM(int n, int* size, int threads_count, int** res) {
         for (int i = start; i <= end; ++i) {
             candidates[i] = 1;
         }
-        // all threads should wait after all numbers get initialized to 1
+        // All threads should wait after all numbers get initialized to 1
         #pragma omp barrier
-        // each thread fill different part of the table, e.g if two threads process N = 25, t0 will start with 2, fill
-        // all numbers that are dividable by 2, and t1 start from 3, when t0 finished with 2, it should call findNext to
-        // get the number 5, and check all numbers dividable by 5. before t0 call findNext should it wait?
+        // Each thread fill different part of the table, e.g if two threads process N = 25, t0 will start with 2, fill
+        // all numbers that are dividable by 2, and t1 start from 3, when t0 finished with 2, it fetch the next prime from queue
         int p = 2;
         while (!is_empty(&q) && p <= n) {
             #pragma omp critical{
@@ -181,13 +134,13 @@ void getPrimesM(int n, int* size, int threads_count, int** res) {
                 }
             }
         }
-        // wait till the table been filled completely, then calculate the count
+        // Wait till the table been filled completely, then calculate the count
         #pragma omp barrier
         for (int i = start + 1; i <= end; ++i) {
             if (i < 2) continue;
             if (candidates[i] == 1) local_count[tid]++;
         }
-        // get total count
+        // Get total count
         #pragma omp barrier
         #pragma omp single
         {
@@ -196,12 +149,13 @@ void getPrimesM(int n, int* size, int threads_count, int** res) {
             }
             *res = (int*)malloc(sizeof(int) * res_count);
             *size = res_count;
-            // copy to res, how to parallelize this part?
-            int k = 0;
-            for (int i = 2; i <= n; ++i) {
-                if (candidates[i] == 1) {
-                    (*res)[k++] = i;
-                }
+        }
+        #pragma omp barrier
+        // Copy to res
+        int k = start;
+        for (int i = start; i <= end; ++i) {
+            if (candidates[i] == 1) {
+                (*res)[k++] = i;
             }
         }
     }
